2cff7a9649045e90d9ad4a2af767c6f8099574f0
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2023-08-21 18:16:59 +0200

    fix: Properly support languages with multiple extensions

---
+++ docs/duplication-tests/tsx-with-results/src/file2.tsx
@@ -0,0 +1,35 @@
+const b = {
+  'a': [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'v',
+    'z',
+  ],
+  'b': [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e'
+  ]
+};
+
+function aFunction(n: number): number {
+  return n + 1
+}
