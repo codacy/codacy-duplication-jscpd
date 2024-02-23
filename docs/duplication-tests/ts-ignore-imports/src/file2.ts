8f5b356e4d195db66e044a16b885b524c651124c
Hélio Rocha <hjrocha@gmail.com>
2024-02-23 12:06:27 +0000

    optimize tests

--- docs/duplication-tests/ts-ignore-imports/src/file2.ts
+++ docs/duplication-tests/ts-ignore-imports/src/file2.ts
@@ -6,38 +6,4 @@ import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { map, filter, switchMap } from 'rxjs/operators';
 
-const b = {
-  'a': [
-    '123a',
-    '123b',
-    '123c',
-    '123d',
-    '123e',
-    '123f',
-    '123g',
-    '123h',
-    '123i',
-    '123l',
-    '123m',
-    '123n',
-    '123o',
-    '123p',
-    '123q',
-    '123r',
-    '123s',
-    '123t',
-    '123v',
-    '123z',
-  ],
-  'b': [
-    '123a',
-    '123b',
-    '123c',
-    '123d',
-    '123e'
-  ]
-};
 
-function aPossibleFunction(n: number): number {
-  return n + 1
-}
